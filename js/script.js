df81825d476398653dab1c4bea5949b558f8032e
Meagan Moore <816moore@gmail.com>
2018-03-13 16:28:21 -0400

    added hover on profile img

--- js/script.js
+++ js/script.js
@@ -2,5 +2,15 @@ $(function () {
     $('.menu a').smoothScroll({
         speed: 800,
         offset: -100
-    });
+    })
+
+    // $('.about-image-container #headshot').hover(function()
+    //     {
+    //         $('#headshot').attr('src', '../assets/derby.jpeg');
+    //     },
+    //     function()
+    //     {
+    //         $('#headshot').attr('src', '../assets/2018-Winter-Bootcamp-Meagan-Moore-3.jpg');
+    // });
 });
+
