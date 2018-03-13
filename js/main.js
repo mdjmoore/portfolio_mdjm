2c1c3536af5728f35135247ec0f138618d002115
Meagan Moore <816moore@gmail.com>
2018-03-13 18:29:36 -0400

    fixed padding and wrapper

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ $(function () {
         speed: 800,
         offset: -100
     });
+
+    // $('.about-image-container #headshot').hover(function()
+    //     {
+    //         $('#headshot').attr('src', '../assets/derby.jpeg');
+    //     },
+    //     function()
+    //     {
+    //         $('#headshot').attr('src', '../assets/2018-Winter-Bootcamp-Meagan-Moore-3.jpg');
+    // });
 });
